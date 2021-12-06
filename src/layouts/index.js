2ee9bb719336091096ee552d9a7d3b13f81ae69a
Stackbit Code Editor <projects@stackbit.com>
2021-12-06 14:33:21 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import _ from 'lodash';
 import moment from 'moment-strftime';
 
-
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { getPageUrl, Link, withPrefix } from '../utils';
