ea54712edcf405cbf8967e5cea82dc1cf345541f
Stackbit Code Editor <projects@stackbit.com>
2021-12-06 14:12:29 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -21,7 +21,7 @@ export default {
     post
 };
 
-export default class Index extends React.Component {
+export default class Index {
     renderPost(post, index, hasMoreLink, moreLinkText) {
         const title = _.get(post, 'title');
         const thumbImage = _.get(post, 'thumb_img_path');
